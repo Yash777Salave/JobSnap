4facc4af7744a3feda6629902654828107dc111f
Yash777Salave <yashsalave777@rediffmail.com>
2024-12-18 10:35:00 +0530

    working on user-profile section, added icons related to basic information

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,31 +1,72 @@
 import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Colors from '../styles/Colors';
 
 const Profile = () => {
   return (
     <View style={styles.MainContainer}>
-      <View style={styles.profileContainer}>
-        <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
-          <View style={styles.ImageContainer}>
+      <View style={{borderWidth: 3}}>
+        <View style={styles.profileContainer}>
+          <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
+            <View style={styles.ImageContainer}>
+              <Image
+                source={require('../assets/yashDiwali.jpg')}
+                style={styles.ProfileImage}
+              />
+            </View>
+
+            <View style={styles.NameContainer}>
+              <Text style={styles.ProfileName}>Yash Salave</Text>
+              <Text style={styles.WorkingAtTXT}>
+                Software Developer DeveloperDeveloperDeveloperDeveloper
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.EditIconContainer}>
             <Image
-              source={require('../assets/yashDiwali.jpg')}
-              style={styles.ProfileImage}
+              source={require('../assets/edit.png')}
+              style={styles.EditIcon}
             />
           </View>
-
-          <View style={styles.NameContainer}>
-            <Text style={styles.ProfileName}>Yash Salave</Text>
-            <Text style={styles.WorkingAtTXT}>
-              Software Developer DeveloperDeveloperDeveloperDeveloper
-            </Text>
-          </View>
         </View>
+        <View style={styles.BasicInfoContainer}>
+          <View style={styles.IconTextContainer}>
+            <Image
+              source={require('../assets/CardIcons/location.png')}
+              style={[styles.Icons, {tintColor: Colors.Grey}]}
+            />
+            <Text style={styles.IconText}>Pune,India</Text>
+          </View>
 
-        <View style={styles.EditIconContainer}>
-          <Image
-            source={require('../assets/edit.png')}
-            style={styles.EditIcon}
-          />
+          <View
+            style={[
+              styles.IconTextContainer,
+              {flexDirection: 'row', borderWidth: 3},
+            ]}>
+            <Image
+              source={require('../assets/CardIcons/mail.png')}
+              style={[styles.Icons, {tintColor: Colors.Grey}]}
+            />
+            <Text style={styles.IconText}>yashsalve999@gmail.com</Text>
+            <View>
+              <Text
+                style={{
+                  color: 'red',
+                  alignSelf: 'flex-end',
+                  borderWidth: 2,
+                }}>
+                Verified
+              </Text>
+            </View>
+          </View>
+          <View style={styles.IconTextContainer}>
+            <Image
+              source={require('../assets/CardIcons/telephone.png')}
+              style={[styles.Icons, {tintColor: Colors.Grey}]}
+            />
+            <Text style={styles.IconText}>+91 7770089444</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -40,7 +81,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   profileContainer: {
-    borderWidth: 2,
+    // borderWidth: 2,
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '90%',
@@ -68,5 +109,20 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     borderWidth: 1,
     width: 200,
+  },
+  BasicInfoContainer: {},
+  Icons: {
+    height: 22,
+    width: 22,
+    color: Colors.Grey,
+  },
+  IconTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  IconText: {
+    fontSize: 14,
+    color: Colors.Grey,
   },
 });
