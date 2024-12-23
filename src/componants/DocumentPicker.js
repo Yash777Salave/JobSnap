a697371952fa8a191340162482924cecf035f250
Yash777Salave <yashsalave777@rediffmail.com>
2024-12-23 20:26:17 +0530

    created separate Document Picker componant

---
+++ src/componants/DocumentPicker.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const DocumentPicker = () => {
+  return (
+    <View>
+      <Text>DocumentPicker</Text>
+    </View>
+  );
+};
+
+export default DocumentPicker;
+
+const styles = StyleSheet.create({});
