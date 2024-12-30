e96e302e2db227c0d3e3569d40c1cc0481cd17c7
Yash777Salave <yashsalave777@rediffmail.com>
2024-12-30 20:30:54 +0530

    working on Document Picker componant for Resume upload

--- src/componants/DocumentPicker.js
+++ src/componants/DocumentPicker.js
@@ -1,14 +1,17 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 import DocumentPicker from 'react-native-document-picker';
+import Colors from '../styles/Colors';
 
 const DocumentPickerr = () => {
+  const [resumeName, setResumeName] = useState();
   const selectDoc = async () => {
     try {
       const doc = await DocumentPicker.pick({
         type: [DocumentPicker.types.pdf],
       });
       const fileName = doc[0].name;
+      setResumeName(fileName);
       console.log('Name of the file is', fileName);
     } catch (error) {
       if (DocumentPicker.isCancel(error)) {
@@ -23,12 +26,22 @@ const DocumentPickerr = () => {
       <View style={styles.CvHeadlineContainer}>
         <View>
           <Text style={styles.HeadlineTXT}>Upload CV</Text>
+          <Text style={styles.tagTXT}>
+            Employers prefer profile with original CV attachment
+          </Text>
         </View>
-        <TouchableOpacity
-          style={{borderWidth: 1, height: 35}}
-          onPress={selectDoc}>
-          <Text>add</Text>
-        </TouchableOpacity>
+        <View style={styles.ResumeContainer}>
+          <Image
+            source={require('../assets/resume.png')}
+            style={styles.EditIcon}
+          />
+          <Text style={styles.ResumeName}>{resumeName}</Text>
+        </View>
+        <View style={styles.AddExpMainContainer}>
+          <TouchableOpacity style={styles.AddExpContainer} onPress={selectDoc}>
+            <Text style={styles.AddExpTXT}>Add </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -52,5 +65,44 @@ const styles = StyleSheet.create({
   HeadlineTXT: {
     fontSize: 21,
     fontWeight: '600',
+  },
+  tagTXT: {
+    fontSize: 17,
+    color: Colors.Grey,
+    // marginBottom: 10,
+  },
+  AddExpContainer: {
+    // borderWidth: 1,
+    width: '45%',
+    alignSelf: 'flex-end',
+    marginTop: 8,
+  },
+  AddExpTXT: {
+    fontSize: 17,
+    color: Colors.Primary_Color,
+    textAlign: 'center',
+    margin: 10,
+  },
+  AddExpMainContainer: {
+    borderTopWidth: 0.4,
+    borderTopEndRadius: 22,
+    borderTopStartRadius: 22,
+  },
+  EditIcon: {
+    height: 90,
+    width: 90,
+    backgroundColor: '#ffff',
+    borderRadius: 10,
+  },
+  ResumeContainer: {
+    // borderWidth: 2,
+    width: '70%',
+    borderRadius: 10,
+    backgroundColor: Colors.lightBlue,
+    padding: 4,
+  },
+  ResumeName: {
+    fontSize: 13,
+    paddingHorizontal: 10,
   },
 });
